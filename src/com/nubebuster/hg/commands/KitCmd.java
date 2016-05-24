e3d0723e14ea2f08a777ffb6c3ed4f139d262d27
NubeBuster <kiwiszijncool@gmail.com>
2016-05-24 23:33:14 +0200

    Update 1.14

--- src/com/nubebuster/hg/commands/KitCmd.java
+++ src/com/nubebuster/hg/commands/KitCmd.java
@@ -4,43 +4,42 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang.WordUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import com.nubebuster.hg.Gamer;
 import com.nubebuster.hg.HG;
 import com.nubebuster.hg.kits.Kit;
 
-public class KitCmd implements CommandExecutor, TabCompleter {
+public class KitCmd implements CommandExecutor, TabCompleter, Listener {
+
+	public KitCmd() {
+		Bukkit.getPluginManager().registerEvents(this, HG.HG);
+	}
 
 	@SuppressWarnings("deprecation")
 	@Override
 	public boolean onCommand(CommandSender sender, Command arg1, String arg2, String[] args) {
-		if (HG.HG.gameTime > -1)
+		Gamer g = Gamer.getGamer((Player) sender);
+		if (HG.HG.gameTime > -1) {
+			sender.sendMessage(ChatColor.RED
+					+ "You cannot choose a kit because your game has already started.\nYou are currently the "
+					+ g.getKit().getKitName() + " kit.");
 			return false;
+		}
 		if (args.length == 0) {
-			String kitss = null;
-			String nop = null;
-			for (Kit k : Kit.kits) {
-				if (!(sender.hasPermission("hg.kits.*")
-						|| sender.hasPermission("hg.kits." + k.getKitName().toLowerCase()))) {
-					if (nop == null)
-						nop = k.getKitName();
-					else
-						nop = nop.concat(", " + k.getKitName());
-				} else {
-					if (kitss == null)
-						kitss = k.getKitName();
-					else
-						kitss = kitss.concat(", " + k.getKitName());
-				}
-			}
-			sender.sendMessage(ChatColor.GREEN + "Your kits: §r" + kitss + "\n§aOther Kits: §r"
-					+ (nop == null ? "You have all kits!" : nop));
+			g.getPlayer().openInventory(createInventory(g.getPlayer()));
 			return false;
 		}
 		if (!(sender instanceof Player))
@@ -56,12 +55,19 @@ public class KitCmd implements CommandExecutor, TabCompleter {
 		}
 		Kit k = Kit.getKitFromName(kit);
 		if (k == null) {
-			sender.sendMessage(ChatColor.RED + "That kit does not exist or is disabled!");
+			sender.sendMessage(ChatColor.RED + "The " + args[0] + " kit does not exist or is disabled!");
 			return false;
 		}
 		Gamer.getGamer(sender.getName()).setKit(k);
 		sender.sendMessage(ChatColor.GREEN + "You are now a " + WordUtils.capitalize(kit) + "!");
 		return false;
+	}
+
+	public Inventory createInventory(Player p) {
+		Inventory i = Bukkit.createInventory(null, 45, "§aKits");
+		for (Kit k : Kit.kits)
+			i.addItem(k.getIconItem());
+		return i;
 	}
 
 	@Override
@@ -90,5 +96,22 @@ public class KitCmd implements CommandExecutor, TabCompleter {
 			kits.add(k.getKitName());
 		kits.remove("None");
 		return kits;
+	}
+
+	@EventHandler
+	public void onClick(InventoryClickEvent event) {
+		if (event.getInventory() == null || !event.getInventory().getTitle().contains("Kits")
+				|| event.getCurrentItem() == null)
+			return;
+		event.setCancelled(true);
+		// Gamer g = Gamer.getGamer((Player) event.getWhoClicked());
+		if (HG.HG.gameTime > 0)
+			return;
+		ItemStack item = event.getCurrentItem();
+		if (item.getItemMeta() != null) {
+			ItemMeta im = item.getItemMeta();
+			Bukkit.dispatchCommand(event.getWhoClicked(), "kit " + im.getDisplayName());
+			event.getWhoClicked().closeInventory();
+		}
 	}
 }
